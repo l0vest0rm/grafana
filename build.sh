e9eb8b6e866e309621dd7bdcbbce49f9a5fd7c98
ning xu <88242590@qq.com>
2020-05-24 07:32:49 +0800

    s

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ basepath=$(cd `dirname $0`; pwd)
 cd $basepath
 
 go run build.go build-server
+if [ $? -eq 0 ]; then
+    echo "build backend success"
+else
+    echo "build backend failed"
+fi
 date
 go run build.go build-frontend
 if [ $? -eq 0 ]; then
@@ -17,8 +22,8 @@ if [ $? -eq 0 ]; then
     mv $basepath/bin/linux-amd64/grafana-server /root/
     scp /root/grafana-server root@172.26.61.202:/root/
     scp /root/grafana-server root@172.26.61.207:/root/
-    echo "succeed"
+    echo "build frontend success"
 else
-    echo "failed"
+    echo "build frontend failed"
 fi
 date
