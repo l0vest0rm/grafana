4dcea7f266d53879384d595ff941bfcac609447c
ning xu <88242590@qq.com>
2020-05-24 07:09:33 +0800

    s

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ if [ $? -eq 0 ]; then
     rm -f public.tgz
     tar czf public.tgz public
     scp $basepath/public.tgz root@172.26.61.202:/root/
-    scp $basepath/public.tgz root@172.26.61.207:/root/
+    #scp $basepath/public.tgz root@172.26.61.207:/root/
     scp $basepath/bin/linux-amd64/grafana-server root@172.26.61.202:/root/
-    scp $basepath/bin/linux-amd64/grafana-server root@172.26.61.207:/root/
+    #scp $basepath/bin/linux-amd64/grafana-server root@172.26.61.207:/root/
     echo "succeed"
 else
     echo "failed"
