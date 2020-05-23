86dfc1e53934e0b5d80bc981aa8b3c05bfc1cef6
ning xu <88242590@qq.com>
2020-05-24 07:11:57 +0800

    s

--- build.sh
+++ build.sh
@@ -8,11 +8,13 @@ go run build.go build-server
 date
 go run build.go build-frontend
 if [ $? -eq 0 ]; then
-    rm -f public.tgz
-    tar czf public.tgz public
+    rm -f /root/public.tgz
+    tar czf /root/public.tgz public
     scp $basepath/public.tgz root@172.26.61.202:/root/
     #scp $basepath/public.tgz root@172.26.61.207:/root/
-    scp $basepath/bin/linux-amd64/grafana-server root@172.26.61.202:/root/
+    rm -f /root/grafana-server
+    mv $basepath/bin/linux-amd64/grafana-server /root/
+    scp /root/grafana-server root@172.26.61.202:/root/
     #scp $basepath/bin/linux-amd64/grafana-server root@172.26.61.207:/root/
     echo "succeed"
 else
