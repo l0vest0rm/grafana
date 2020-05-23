402bc3b17cf7f1cb6582714517a1028d21f7519c
ning xu <88242590@qq.com>
2020-05-23 19:36:57 +0800

    s

--- build.sh
+++ build.sh
@@ -6,5 +6,14 @@ cd $basepath
 
 go run build.go build-server
 go run build.go build-frontend
-rm -f public.tgz
-tar czf public.tgz public
+if [ $? -eq 0 ]; then
+    rm -f public.tgz
+    tar czf public.tgz public
+    scp $basepath/public.tgz root@172.26.61.202:/root/
+    scp $basepath/public.tgz root@172.26.61.207:/root/
+    scp $basepath/bin/linux-amd64/grafana-server root@172.26.61.202:/root/
+    scp $basepath/bin/linux-amd64/grafana-server root@172.26.61.207:/root/
+    echo "succeed"
+else
+    echo "failed"
+fi
